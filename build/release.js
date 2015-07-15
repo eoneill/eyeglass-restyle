e4f95a27cfdb81abd9b76413ade689247b10b423
Eugene ONeill <oneill.eugene@gmail.com>
2015-07-15 01:09:17 -0700

    updating release task

--- build/release.js
+++ build/release.js
@@ -2,12 +2,17 @@
 
 var git = require("gulp-git");
 var bump = require("gulp-bump");
+var prompt = require("gulp-prompt");
 var tagVersion = require("gulp-tag-version");
+
+var pkgSource = ["./package.json"];
+
+var versionTypes = ["patch", "minor", "major"];
 
 module.exports = function(gulp, depends) {
   function increment(type) {
     // get all the files to bump version in
-    return gulp.src(["./package.json"])
+    return gulp.src(pkgSource)
       // bump the version number in those files
       .pipe(bump({type: type}))
       // save it back to filesystem
@@ -18,7 +23,22 @@ module.exports = function(gulp, depends) {
       .pipe(tagVersion());
   }
 
-  gulp.task("release:patch", depends, increment.bind("patch"));
-  gulp.task("release:minor", depends, increment.bind("minor"));
-  gulp.task("release:major", depends, increment.bind("major"));
+  gulp.task("release", depends, function(done) {
+    return gulp.src(pkgSource)
+      .pipe(prompt.prompt({
+          type: "checkbox",
+          name: "type",
+          message: "What type of release would you like to do?",
+          choices: versionTypes
+      }, function(result){
+        var type = result.type[0];
+        if (type) {
+          return increment(type);
+        }
+      }));
+  });
+
+  versionTypes.forEach(function(version) {
+    gulp.task("release:" + version, depends, increment.bind(increment, version));
+  });
 };
