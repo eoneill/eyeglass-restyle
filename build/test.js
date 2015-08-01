42f3ebcf42e0af7372aa5264cce12c4af06e3ea9
Eugene ONeill <oneill.eugene@gmail.com>
2015-08-01 11:56:47 -0700

    merge test and coverage tasks

--- build/test.js
+++ build/test.js
@@ -1,12 +1,32 @@
 "use strict";
 
+var istanbul = require("gulp-istanbul");
 var mocha = require("gulp-mocha");
 
-module.exports = function(gulp, depends) {
-  gulp.task("test", depends, function() {
-    return gulp.src([
-      "test/**/test_*.js"
-      ], {read: false})
-        .pipe(mocha({reporter: "spec"}));
+module.exports = function(gulp, depends, options) {
+  options = options || {};
+  gulp.task("test", depends, function (cb) {
+    gulp.src(["lib/**/*.js"])
+    .pipe(istanbul()) // Covering files
+    .pipe(istanbul.hookRequire()) // Force `require` to return covered files
+    .on("finish", function () {
+      gulp.src(["test/**/test_*.js"], {
+        read: false
+      })
+      .pipe(mocha({
+        reporter: "spec"
+      }))
+      .pipe(istanbul.writeReports()) // Creating the reports after tests runned
+      .pipe(istanbul.enforceThresholds({
+        thresholds: options.coverage && options.coverage.thresholds
+      }))
+      .on("end", cb)
+      .on("error", function(e) {
+        console.error(e.toString());
+        /*eslint-disable */
+        process.exit(1);
+        /*eslint-enable */
+      });
+    });
   });
 };
--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,26 @@
 
 var gulp = require("gulp");
 
-require("./build/lint")(gulp);
-require("./build/test")(gulp, ["lint"]);
 require("./build/sassdoc")(gulp);
 require("./build/docco")(gulp);
 require("./build/jsdoc")(gulp);
+
 require("./build/release")(gulp);
 require("./build/publish")(gulp);
-require("./build/coverage")(gulp);
+
+require("./build/lint")(gulp);
+require("./build/test")(gulp, ["lint"], {
+  coverage: {
+    thresholds: {
+      global: {
+        //statements: 95.05,
+        //branches: 86.64,
+        //functions: 97.14,
+        //lines: 95.05
+      }
+    }
+  }
+});
 
 gulp.task("default", ["test"]);
 
